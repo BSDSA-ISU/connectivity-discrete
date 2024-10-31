d44ed812095ee4e1af3339af24eeb9a74206867c
troy600 <bazartroy531@gmail.com>
2024-10-31 14:42:33 +0800

    update

--- or.java
+++ or.java
@@ -1,3 +1,5 @@
+import lib.classes;
+
 public class or {
     public static void main(String[] args) {
         java.util.Scanner input = new java.util.Scanner(System.in);
@@ -15,7 +17,7 @@ public class or {
             }
             input.close();
             for (int i=0; i<a.length; i++) {
-                System.out.println("formula: ("+a[i]+" V "+b[i] + ") " + " the " + (i+1)+ "aV" +  + (i+1)+"b Is: " + classes.or(a[i], b[i]));
+                System.out.println("formula: ("+a[i]+" V "+b[i] + ") " + " the " + (i+1)+ "a V " +  + (i+1)+"b Is: " + classes.or(a[i], b[i]));
             }
 
         } catch (java.util.InputMismatchException e) {
